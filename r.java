321ad1b8a253c2d5e5790d26f4a280ce90f5d6a8
Hannah <hmt17@pitt.edu>
2016-12-04 15:51:28 -0500

    Update r.java

--- r.java
+++ r.java
@@ -1,45 +1,46 @@
-//reducer
 import java.io.IOException;
 import java.util.*;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class R extends Reducer<Text, Text, Text, Text> {
+public class R extends Reducer<Text, docOffsets, Text, Text> {
 
 	@Override
-	public void reduce(final Text key, final Iterable<Text> values,
+	public void reduce(final Text key, final Iterable<docOffsets> values,
 			final Context context) throws IOException, InterruptedException {
 
-		HashMap<String, Integer> m=new HashMap<String, Integer>();
-		StringBuilder stringBuilder = new StringBuilder();
-		Iterable<Map.Entry<String, Integer>> keySet;
-		int count =0;
+		// HashMap<String, Integer> m=new HashMap<String, Integer>();
+		// StringBuilder stringBuilder = new StringBuilder();
+		// Iterable<Map.Entry<String, Integer>> keySet;
+		// int count =0;
 		
-		for (Text value : values) {
-			String str = value.toString();
+		// for (Text value : values) {
+			// String str = value.toString();
 
-			if(m.containsKey(str)){
+			// if(m.containsKey(str)){
  
- 				//count=(int)m.get(str);
+ 				// //count=(int)m.get(str);
 				 
-				m.put(str, m.get(str)+1);
+				// m.put(str, m.get(str)+1);
 				 
-			}else{
-				m.put(str, 1);
-			}
-		}
-		keySet = m.entrySet();
-		//keySet = m.entrySet().iterator();
-		//java.util.Iterator keySet = m.keySet().iterator();
-		for(Map.Entry<String, Integer> keys: keySet){
-			stringBuilder.append(keys.getKey() + "\\t" + keys.getValue());
-			if (keySet.iterator().hasNext()) {
-				stringBuilder.append("\\t");
-			}
-		}
-
-		context.write(key,new Text( m.toString()));
+			// }else{
+				// m.put(str, 1);
+			// }
+		// }
+		// keySet = m.entrySet();
+		// //keySet = m.entrySet().iterator();
+		// //java.util.Iterator keySet = m.keySet().iterator();
+		// for(Map.Entry<String, Integer> keys: keySet){
+			// stringBuilder.append(keys.getKey() + "\\t" + keys.getValue());
+			// if (keySet.iterator().hasNext()) {
+				// stringBuilder.append("\\t");
+			// }
+		// }
+		for (docOffsets value : values) {
+			String str2 = value.filename + " " + value.offset;
+		context.write(key,new Text( str2.toString()));
 		 //new Text(stringBuilder.toString()));
+		}
 	}
 
 }

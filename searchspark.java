27d744c5ab00a8873e882984ab25887a8b8573e1
Hannah <hmt17@pitt.edu>
2016-12-11 20:21:38 -0500

    Update searchspark.java

--- searchspark.java
+++ searchspark.java
@@ -30,3 +30,21 @@ public class searchspark{
 
 	}
 }
+
+
+/**
+Some links that I've been looking at - i think java is better but a few people are using all python for this
+
+python:
+https://pymotw.com/2/subprocess/
+http://stackoverflow.com/questions/40027207/python-subprocess-module-hangs-for-spark-submit-command-when-writing-stdout
+http://stackoverflow.com/questions/16768290/understanding-popen-communicate
+
+java:
+https://stackoverflow.com/questions/26817940/how-to-use-map-function-in-spark-with-java
+http://stackoverflow.com/questions/11352037/scala-utc-timestamp-in-seconds-since-january-1st-1970
+https://www.tutorialspoint.com/java/lang/runtime_exec_envp.htm
+http://alvinalexander.com/scala/scala-import-java-classes-packages-examples
+https://stackoverflow.com/questions/33265056/how-do-i-determine-an-offset-in-apache-spark#33321491
+https://stackoverflow.com/questions/33265056/how-do-i-determine-an-offset-in-apache-spark
+**/

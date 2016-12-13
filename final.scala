40914e8339c587d601a4926b53708d761b7f00e2
eric-larkin <eal64@pitt.edu>
2016-12-12 22:40:46 -0500

    Update final.scala

--- final.scala
+++ final.scala
@@ -8,7 +8,7 @@ import org.apache.spark.mllib.linalg.distributed._
 import java.io._
 import java.io.File
 import collection.JavaConverters._
-//import com.cloud._ this is for the parser
+//import com.cloud._
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
@@ -16,10 +16,22 @@ import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 import org.apache.hadoop.io.Text;
 
+
+class filec(val filen: String, val offsets:List[Long]) extends Comparable[filec] with Serializable {
+    override def compareTo(o:filec):Int = {
+        return this.filen.compareTo(o.filen)
+    }
+
+    override def toString: String = {
+        "{[" + filen + "][" + offsets.mkString(",") + "]}"
+    }
+};
+
 object Main {
     def main(args: Array[String]) {
         val conf = new SparkConf().setAppName("Final")
         //val sc = new SparkContext(conf)
+
 
 
         /* this needs to be a method not a function b/c by itself it is not serializable and will error out! */
@@ -37,29 +49,49 @@ object Main {
         println("Hello world")
 
         val inputPath = "/user/djflash/input/"
+        val outputPath = "/user/djflash/output/scalaout"
         val fs = FileSystem.get(new Configuration())
         val status = fs.listStatus(new Path(inputPath))
+        val output = fs.create(new Path(outputPath))
+        val out = new BufferedOutputStream(output)
+        var all:RDD[(String, filec)] = sc.emptyRDD[(String, filec)]
 
         status.foreach(x=> {
             val path:Path = x.getPath
             val filename:String = path.getName
 
-            val lines:RDD[Tuple2[Long, String]] = sc.newAPIHadoopFile(inputPath + filename, classOf[TextInputFormat], classOf[LongWritable],classOf[Text], sc.hadoopConfiguration).map{ case (x:LongWritable, y:Text) => (x.get,y.toString) }
-            val pairsPerLine = lines.map{case (x:Long, y:String) => func(x,y)}
-            val pairs = pairsPerLine.flatMap(y => y)
-val fullPairs = pairs.map{case(a:Long,b:String) => { println(filename + " " + a.toString + " " + b); (filename,a,b)}}
+            var lines:RDD[Tuple2[Long, String]] = sc.newAPIHadoopFile(inputPath + filename, classOf[TextInputFormat], classOf[LongWritable],classOf[Text], sc.hadoopConfiguration).map{ case (x:LongWritable, y:Text) => (x.get,y.toString) }
+            var pairsPerLine = lines.map{case (x:Long, y:String) => func(x,y)}
+            var pairs = pairsPerLine.flatMap(y => y)
+            var fullPairs = pairs.map{case(a:Long,b:String) => { println(filename + " " + a.toString + " " + b); (filename,a,b)}}
             fullPairs.collect().foreach(println)
 
+            var separateOffsets = fullPairs.map({case (filename, offset, word) => (word, filename) -> offset})
+            var combineOffsets = separateOffsets.groupByKey()
+            var properForm = combineOffsets.map{case(x,y) => x._1 -> new filec(x._2, y.toList)}
+            all = all.union(properForm)
         })
+        println("\r\n\r\nDone with files. combining....\r\n\r\n")
 
+        var combined = all.groupByKey()
 
+        combined.collect().foreach{case(word, iter) => {
+                out.write(word.getBytes("UTF-8"))
+                out.write("-".getBytes("UTF-8"))
+                iter.foreach(x => out.write(x.toString.getBytes("UTF-8")))
+                out.write("\r\n".getBytes("UTF-8"))
+            }
+        }
 
-        /* works! this is for the parser
-        val parser = new LineParser("hdfs:///user/djflash/output/part-r-00000", "computer")
+        out.close()
+/*
+        println("\r\n Parser output - searching for 'computer'")
+
+        /* works! this is for the parser */
+        val parser = new LineParser("hdfs://" + outputPath, "computer")
         parser.parse()
         val result = parser.parserResult.asScala
-        println("Hello world")
         result.foreach(a => println(a))
-        */
+*/
     }
 }

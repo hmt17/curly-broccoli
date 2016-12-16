23b2ce9058e7e3668f9e2306b0dcfabedad3d81f
Hannah <hmt17@pitt.edu>
2016-12-15 21:03:18 -0500

    Update final.scala

--- final.scala
+++ final.scala
@@ -1,12 +1,13 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import scala.io.Source
+import scala.io._
 import org.apache.spark.rdd._
 import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.linalg.distributed._
 import java.io._
 import java.io.File
+import java.sql.Timestamp;
 import collection.JavaConverters._
 import com.cloud._
 import org.apache.hadoop.fs.Path
@@ -31,9 +32,42 @@ object Main {
     def main(args: Array[String]) {
         val conf = new SparkConf().setAppName("Final")
         val sc = new SparkContext(conf)
-
-
-
+		
+		var exit = false;
+		while(exit){
+			println("Please input a command:")
+			println("1 = Index \t 2 = Search word(s) \t 3 = Search phrase \t 4 = Quit")
+			val ln = readLine()
+			
+			if(ln = 1){
+			//index 
+				val indexCompleted = RunIndex(args);				
+				if(!indexCompleted) {
+					System.exit(1)
+				}
+			}
+			else if(ln = 2){
+			//search word(s)
+				Search(0)
+			}
+			else if(ln = 3){
+			//search phrase
+				Search(1)
+			}
+			else if(ln = 4){
+			//quit
+				exit = true
+			}
+			else{
+			//wrong input
+				println("Please enter valid input")
+			}
+		}
+		System.exit(1)
+	}
+		
+	def RunIndex() {
+        val startTime = new java.sql.Timestamp(date.getTime)
         /* this needs to be a method not a function b/c by itself it is not serializable and will error out! */
         val func = (line:(Long,String)) => {
             val words = line._2.split(" ");
@@ -46,10 +80,11 @@ object Main {
             )
             test
         }
+		
         println("Hello world")
 
-        val inputPath = "/user/djflash/input/"
-        val outputPath = "/user/djflash/output/scalaout"
+        val inputPath = "/user/hmt17/input/"
+        val outputPath = "/user/hmt17/output/scalaout"
         val fs = FileSystem.get(new Configuration())
         val status = fs.listStatus(new Path(inputPath))
         val output = fs.create(new Path(outputPath))
@@ -84,14 +119,25 @@ object Main {
         }
 
         out.close()
-
-        println("\r\n Parser output - searching for 'computer'")
+		val endTime = new java.sql.Timestamp(date.getTime)
+		println("Difference: " + (endTime - startTime))
+	}
+     def Search(wordOrPhrase: Int) {
+		val startTime = new java.sql.Timestamp(date.getTime)
+		
+		if(
+		val input = readLine("Please enter the words to search (seperated by a single space):")
+		
+		println("\r\n Parser output - searching for '" + input + "'")
 
         /* works! this is for the parser */
-        val parser = new LineParser("hdfs://" + outputPath, "computer")
+        val parser = new LineParser("hdfs://" + outputPath, input)
         parser.parse()
         val result = parser.parserResult.asScala
         result.foreach(a => println(a))
 
+		val endTime = new java.sql.Timestamp(date.getTime)
+		println("Difference: " + (endTime - startTime))
     }
 }
+System.exit(0)
